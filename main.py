c876ad8670a474958f3a00ee54f55a3429422541
Zhang-hoon Dennis Oh <zhanghoondennisoh@gmail.com>
2025-03-06 02:38:25 +0900

    now one removes url
    1. Now one removes URLs in text.
    2. One replaced a single-quote with a double-quote.
    
    Signed-off-by: Zhang-hoon Dennis Oh <zhanghoondennisoh@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-'''
+"""
 This module get contents from the web site,
 then send that content to a telegram chat room with a bot.
-'''
+You cannot use requests and BeautifulSoup only. Use of selenium is required to get the content from the web site - "fmkorea.com."
+"""
 
 import datetime
 import re
@@ -14,6 +15,7 @@ from bs4 import BeautifulSoup
 from loguru import logger
 import requests
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver as Chrome
 import selenium
 import yaml
 
@@ -50,11 +52,11 @@ class LinkVisitorClientContext:
             self.driver.quit()
 
 
-def visit_page(driver, url):
+def visit_page(driver: Chrome, url: str) -> None:
     try:
         driver.get(url)
     except selenium.common.exceptions.WebDriverException as e:
-        logger.error(f'Error visiting {url}')
+        logger.error(f"Error visiting {url}")
         logger.error(e)
         sys.exit(-1)
 
@@ -68,60 +70,83 @@ def create_client_context_with_selenium() -> LinkVisitorClientContext:
     return client_context
 
 
-def visit_with_selenium(client_context, url) -> None:
+def visit_with_selenium(client_context: LinkVisitorClientContext, url: str) -> None:
     visit_page(client_context.driver, url)
 
 
+def remove_video_tag_message(text: str) -> str:
+    return text.replace("Video 태그를 지원하지 않는 브라우저입니다.", "")
+
+
+def remove_urls(text: str) -> str:
+    url_pattern = r"https?://\S+|www\.\S+"
+    return re.sub(url_pattern, "", text)
+
+
 def remove_any_unused_text(text: str) -> str:
-    return text.replace('Video 태그를 지원하지 않는 브라우저입니다.', '')
+    text = remove_video_tag_message(text)
+    text = remove_urls(text)
+    return text
 
 
-def visit_article_link(context: dict, client_context: LinkVisitorClientContext, href: str) -> tuple[bool, str]:
+def visit_article_link(
+    context: dict, client_context: LinkVisitorClientContext, href: str
+) -> tuple[bool, str]:
     flag_continue = False
     text = None
 
     if href:
-        if context['visited_item_recorder'].is_visited(href):
-            logger.info(f'Already visited: ({href}). Skip it.')
+        if context["visited_item_recorder"].is_visited(href):
+            logger.info(f"Already visited: ({href}). Skip it.")
             flag_continue = True
         else:
-            context['visited_item_recorder'].add_item(href)
-            url_for_href = f'https://www.fmkorea.com{href}'
+            context["visited_item_recorder"].add_item(href)
+            url_for_href = f"https://www.fmkorea.com{href}"
             const_time_to_sleep_after_visit_using_selenium = 2
             const_time_to_sleep_between_req_for_href_in_sec = 1
             const_timeout_for_requests_get_in_sec = 16
 
             try:
                 visit_with_selenium(client_context, url_for_href)
-                context['exit_event'].wait(const_time_to_sleep_after_visit_using_selenium)
-                req_for_href = requests.get(url_for_href, timeout=const_timeout_for_requests_get_in_sec)
-                context['exit_event'].wait(const_time_to_sleep_between_req_for_href_in_sec)
-                soup_for_href = BeautifulSoup(req_for_href.content, "html.parser", from_encoding='cp949')
-                if div_tags := soup_for_href.find_all('div', 'xe_content'):
+                context["exit_event"].wait(
+                    const_time_to_sleep_after_visit_using_selenium
+                )
+                req_for_href = requests.get(
+                    url_for_href, timeout=const_timeout_for_requests_get_in_sec
+                )
+                context["exit_event"].wait(
+                    const_time_to_sleep_between_req_for_href_in_sec
+                )
+                soup_for_href = BeautifulSoup(
+                    req_for_href.content, "html.parser", from_encoding="cp949"
+                )
+                if div_tags := soup_for_href.find_all("div", "xe_content"):
                     if div_tags[0] and div_tags[0].text:
                         text = div_tags[0].text.strip()
                         text = remove_any_unused_text(text)
             except Exception:
-                context['exit_event'].wait(const_time_to_sleep_between_req_for_href_in_sec)
+                context["exit_event"].wait(
+                    const_time_to_sleep_between_req_for_href_in_sec
+                )
 
     return (flag_continue, text)
 
 
-def get_message_to_send(context) -> str:
+def get_message_to_send(context: dict) -> str:
     const_time_to_sleep_after_visit_using_selenium = 2
     const_timeout_for_requests_get_in_sec = 16
 
-    to_return = ''
+    to_return = ""
     page_number = 1
-    url = f'https://www.fmkorea.com/index.php?mid=football_world&page={page_number}'
+    url = f"https://www.fmkorea.com/index.php?mid=football_world&page={page_number}"
     client_context = create_client_context_with_selenium()
     visit_with_selenium(client_context, url)
-    context['exit_event'].wait(const_time_to_sleep_after_visit_using_selenium)
+    context["exit_event"].wait(const_time_to_sleep_after_visit_using_selenium)
     req = requests.get(url, timeout=const_timeout_for_requests_get_in_sec)
-    soup = BeautifulSoup(req.content, "html.parser", from_encoding='cp949')
-    td_tags = soup.find_all('td', 'title hotdeal_var8')
+    soup = BeautifulSoup(req.content, "html.parser", from_encoding="cp949")
+    td_tags = soup.find_all("td", "title hotdeal_var8")
 
-    logger.info(f'Number of tags: ({len(td_tags)})')
+    logger.info(f"Number of tags: ({len(td_tags)})")
 
     const_max_td_tags = 20
     limit_number = max(const_max_td_tags, len(td_tags))
@@ -129,24 +154,26 @@ def get_message_to_send(context) -> str:
         td_tag = td_tags[i]
 
         # Let's look for a |category|.
-        category = ''
+        category = ""
         tr_tag_for_article_item = td_tag.parent
-        td_tag_for_category = tr_tag_for_article_item.find('td', 'cate')
-        first_a_tag_for_category = td_tag_for_category.find('a')
+        td_tag_for_category = tr_tag_for_article_item.find("td", "cate")
+        first_a_tag_for_category = td_tag_for_category.find("a")
         if first_a_tag_for_category is not None:
             category = first_a_tag_for_category.text.strip()
 
         # Let's look for a |title| and |text|.
-        title = ''
-        text = ''
+        title = ""
+        text = ""
         if td_tag is not None:
-            a_tags = td_tag.find_all('a')
+            a_tags = td_tag.find_all("a")
             first_a_tag = a_tags[0]
             if first_a_tag is not None:
                 title = first_a_tag.text.strip()
                 # print_message(str(title))
-                href = first_a_tag['href']
-                (continue_flag, text) = visit_article_link(context, client_context, href)
+                href = first_a_tag["href"]
+                (continue_flag, text) = visit_article_link(
+                    context, client_context, href
+                )
                 if continue_flag:
                     continue
 
@@ -156,11 +183,11 @@ def get_message_to_send(context) -> str:
         title = escape_text(title)
         if text:
             text = escape_text(text)
-            logger.info(f'- [{category}]{title} ({text})')
-            to_return = f'{to_return}- \\[{category}]{title} ({text})\n'
+            logger.info(f"- [{category}]{title} ({text})")
+            to_return = f"{to_return}- \\[{category}]{title} ({text})\n"
         else:
-            logger.info(f'- [{category}]{title}')
-            to_return = f'{to_return}- \\[{category}]{title}\n'
+            logger.info(f"- [{category}]{title}")
+            to_return = f"{to_return}- \\[{category}]{title}\n"
 
     client_context.clean_up()
     return to_return
@@ -168,93 +195,97 @@ def get_message_to_send(context) -> str:
 
 def print_message(message: str):
     if message is not None:
-        sys.stdout.buffer.write(message.encode('utf-8'))
-        sys.stdout.buffer.write(('\n').encode('utf-8'))
+        sys.stdout.buffer.write(message.encode("utf-8"))
+        sys.stdout.buffer.write(("\n").encode("utf-8"))
         sys.stdout.buffer.flush()
 
 
-def run_loop_with_context(context):
+def run_loop_with_context(context: dict):
     const_time_to_sleep_between_req = 60
     max_count = 120
     for _ in range(max_count):
-        logger.info('Trying to fetch content...')
+        logger.info("Trying to fetch content...")
         message_to_send = get_message_to_send(context)
         if len(message_to_send) > 0:
             send_telegram_message(context, message_to_send)
         logger.info(datetime.datetime.now())
-        logger.info('Now sleep...')
+        logger.info("Now sleep...")
         for _ in range(const_time_to_sleep_between_req):
-            context['exit_event'].wait(1)
+            context["exit_event"].wait(1)
         logger.info(datetime.datetime.now())
 
 
-def quit_application(signo, _frame, context):
+def quit_application(signo, _frame, context: dict):
     # `_frame` and `context` are not used. It's intentional.
-    logger.info(f'Interrupted by signal number {signo}, shutting down...')
+    logger.info(f"Interrupted by signal number {signo}, shutting down...")
     logger.info(_frame)
     logger.info(context)
-    logger.info(context['exit_event'])
+    logger.info(context["exit_event"])
     sys.exit(-1)
 
 
 def init_context_with_global_config(context: dict, global_config: dict) -> None:
-    context['bot_token'] = global_config['bot_token']
-    context['bot_chat_id'] = global_config['bot_chat_id']
+    context["bot_token"] = global_config["bot_token"]
+    context["bot_chat_id"] = global_config["bot_chat_id"]
 
-    context['visited_item_recorder'] = VisitedItemRecorder([])
+    context["visited_item_recorder"] = VisitedItemRecorder([])
 
 
-def init_signal_functions(context) -> None:
-    '''
+def init_signal_functions(context: dict) -> None:
+    """
     This function set up signal handlers.
     Also it sets `context`'s key, value.
-    '''
-    context['exit_event'] = Event()
-    signal.signal(signal.SIGTERM, lambda signo, frame: quit_application(signo, frame, context))
-    signal.signal(signal.SIGINT, lambda signo, frame: quit_application(signo, frame, context))
+    """
+    context["exit_event"] = Event()
+    signal.signal(
+        signal.SIGTERM, lambda signo, frame: quit_application(signo, frame, context)
+    )
+    signal.signal(
+        signal.SIGINT, lambda signo, frame: quit_application(signo, frame, context)
+    )
 
 
-def run_loop_with_global_config(global_config):
+def run_loop_with_global_config(global_config: dict) -> None:
     context = {}
     init_context_with_global_config(context, global_config)
     init_signal_functions(context)
     run_loop_with_context(context)
 
 
-def escape_text(text):
+def escape_text(text: str) -> str:
     # const_regex_to_escape = r"(?<!\\)(_|\*|\[|\]|\(|\)|\~|`|>|#|\+|-|=|\||\{|\}|\.|\!)"
     const_regex_to_escape = r"(?<!\\)(_|\*|\[|\]|\(|\)|\~|`|>|#|\+|=|\||\{|\})"
-    text = re.sub(const_regex_to_escape, lambda t: "\\"+t.group(), text)
+    text = re.sub(const_regex_to_escape, lambda t: "\\" + t.group(), text)
     return text
 
 
 def send_telegram_message(context, message: str) -> None:
     const_timeout_for_requests_get_in_sec = 16
 
-    bot_token = context['bot_token']
-    bot_chat_id = context['bot_chat_id']
-    url = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={bot_chat_id}&parse_mode=Markdown&text={message}'
+    bot_token = context["bot_token"]
+    bot_chat_id = context["bot_chat_id"]
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={bot_chat_id}&parse_mode=Markdown&text={message}"
     requests.get(url, timeout=const_timeout_for_requests_get_in_sec)
 
 
-def validate_global_config(global_config):
-    if 'bot_token' not in global_config:
+def validate_global_config(global_config: dict):
+    if "bot_token" not in global_config:
         return False
-    if 'bot_chat_id' not in global_config:
+    if "bot_chat_id" not in global_config:
         return False
-    default_bot_token_for_template = '12345:YOUR FULL BOT TOKEN'
-    return global_config['bot_token'] != default_bot_token_for_template
+    const_default_bot_token_for_template = "12345:YOUR FULL BOT TOKEN"
+    return global_config["bot_token"] != const_default_bot_token_for_template
 
 
 def load_config_and_run_loop():
     try:
-        with open('global_config.yaml', 'rb') as config_file_stream:
+        with open("global_config.yaml", "rb") as config_file_stream:
             global_config = yaml.safe_load(config_file_stream)
     except IOError as e:
-        logger.error(f'An IOError has been occurred: {e}')
+        logger.error(f"An IOError has been occurred: {e}")
         sys.exit(-1)
     if not validate_global_config(global_config):
-        logger.error('Error in the global configuration')
+        logger.error("Error in the global configuration")
         sys.exit(-1)
     run_loop_with_global_config(global_config)
 
@@ -263,5 +294,5 @@ def main():
     load_config_and_run_loop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
